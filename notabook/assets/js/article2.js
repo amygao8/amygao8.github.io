384d93af6dbb904c65080e4c666636bf627eea63
Amy Gao <amy@Amys-MBP.hub>
2020-11-20 22:02:56 +1100

    notabook

---
+++ notabook/assets/js/article2.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+	console.log('hello world');
+
+
+$('.icon').click(function(){
+	$('.quote').toggleClass('active');
+	$('.pics').toggleClass('active');
+	});
+
+$('.boti').hover(function(){
+	$('.info').toggleClass('active');
+	});
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("menu").style.top = "0";
+  } else {
+    document.getElementById("menu").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+});
