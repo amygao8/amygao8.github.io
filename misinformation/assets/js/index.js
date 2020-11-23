72b850851a73f478aef723342aa5e272c727f356
Amy Gao <amy@Amys-MBP.hub>
2020-11-24 00:19:48 +1100

    change

---
+++ misinformation/assets/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	console.log('hello world');
+
+
+$('.top').click(function(){
+	$('.top').toggleClass('hide');
+  $('.lead').toggleClass('active');
+  $('.intro').toggleClass('active');
+
+
+
+	});
+
+
+});
+
